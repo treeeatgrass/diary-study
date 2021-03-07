733c93d8a55b4457d5bb3eaff28c34c6f74fa871
treeeatgrass <63838596+treeeatgrass@users.noreply.github.com>
2021-03-07 16:21:05 -0500

    Update statistical modeling.R

--- statistical modeling.R
+++ statistical modeling.R
@@ -7,19 +7,19 @@ m0 <- glmer(device5 ~ (1|ID),
               control = glmerControl(optimizer = "bobyqa"),
               nAGQ = 0)
 #Step 1: add level1 variables with covariate
-m1 <- glmer(device5 ~ UnhappySocial + POS5.2 + NEG5.2 +(1|ID), 
+m1 <- glmer(device5 ~ UnhappySocial + Cpos + Cneg +(1|ID), 
               data = df, family = binomial(link = "logit"),
               control = glmerControl(optimizer = "bobyqa"),
               nAGQ = 0)
 #Step 2: add level2 variables to test main effects
-m2 <- glmer(device5 ~ UnhappySocial + POS5.2 + NEG5.2 + CsoAnxiety + (1|ID),
+m2 <- glmer(device5 ~ UnhappySocial + Cpos + Cneg + CsoAnxiety + (1|ID),
               data = df, family = binomial(link = "logit"),
               control = glmerControl(optimizer = "bobyqa"),
               nAGQ = 0)
 #Step 3: add cross-level interaction terms
 m3 <- glmer(device5 ~ UnhappySocial + 
-                POS5.2 + NEG5.2 + CsoAnxiety +
-                POS5.2*CsoAnxiety + NEG5.2*CsoAnxiety + (1|ID),
+                Cpos + Cneg + CsoAnxiety +
+                Cpos*CsoAnxiety + Cneg*CsoAnxiety + (1|ID),
               data = df, family = binomial(link = "logit"),
               control = glmerControl(optimizer = "bobyqa"),
               nAGQ = 0)  
@@ -28,4 +28,4 @@ m3 <- glmer(device5 ~ UnhappySocial +
 anova(m0, m1)
 anova(m1, m2)
 anova(m2, m3)
-summary(m3)
+summary(m1)
